2f754f0731f0b9091b7d305f18d8b727fdae28cb
samaty59 <you@example.com>
2023-06-08 01:58:26 +0200

    routing done

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
-const { MongoClient } = require("mongodb");
 const express = require("express");
-
-
-
-const uri = "mongodb+srv://abdelboussaboun:uyZQLIVDBqq3cN4i@cluster0.qgdicmo.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri);
-
-async function run() {
-    try {
-      const database = client.db('pangolin');
-      const lists = database.collection('profils');
-      const data = await lists.findOne();
-      console.log(data);
-    } finally {
-        await client.close();
-    }
-  }
-  run().catch(console.dir);
-
-
-
 const app = express();
+var cors = require('cors')
+
+const todoRoutes = require("./routes/todo")
+
+
+
+
+
 
 app.get("/", (req, res) => {
     res.send("hello world");
 });
+
+// For parsing application/json
+app.use(express.json());
+ 
+// For parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+app.use("/api", todoRoutes);
 
 app.listen(8080, () => {
     console.log("listening on port 8080");
--- routes/todo.js
+++ routes/todo.js
@@ -2,5 +2,6 @@ const express = require("express");
 const router = express.Router();
 const todoController = require("../controlers/todo")
 
+router.get("", todoController.getMyProfilInfo);
 
 module.exports = router;
